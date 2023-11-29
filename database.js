f249465f7fbed441babd6dd43a8488a1fa7e33bf
Indah Fatimah Afriani <121292909+indahfafriani@users.noreply.github.com>
2023-11-30 00:59:02 +0700

    indah ini ya

---
+++ database.js
@@ -0,0 +1,11 @@
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'password',
+      database : 'revou'
+    }
+  });
+  module.exports = knex
